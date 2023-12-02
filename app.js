5852a5d46c94d3b05921fc626898158a829cb1c0
@yogesh4343 <yogeshsinghal2001@gmail.com>
2023-12-03 00:47:16 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+require('dotenv').config();
+const connectDb = require('./db/connect');
+const api_router = require('./routes/apiRoutes');
+
+const cors = require('cors');
+
+
+
+
+const app = express()
+
+// app.use(cors());
+app.use(cors({
+    "origin":"http://localhost:3000/"
+}));
+
+
+// var router = express.Router();
+const PORT = process.env.PORT || 4000
+
+
+
+
+app.get('/' , (req, res) => {
+    res.send("hii live code")
+
+})
+
+// middleware 
+app.use('/api/' , api_router);
+
+
+
+// connect 
+const start = async() => {
+    try {
+        await connectDb(process.env.MONGODB_URL);
+
+        app.listen(PORT  , ()=>{
+           console.log( `${PORT} is conn`)
+        })
+    } catch (error) {
+            console.log(error)
+    }
+}
+start()
---
+++ models/apiModels.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose');
+
+
+const productSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    instructor : {
+        type:String,
+        required:[true, "Introduction to React Native"]
+    },
+    description : {
+        type:String,
+        required:true
+    },
+    enrollmentStatus : {
+        type:String,
+        required:true 
+    },
+    thumbnail : {
+        type:String,
+        required:true 
+    },
+    duration : {
+        type:String,
+        required:true 
+    },
+    schedule : {
+        type:String,
+        required:true 
+    },
+    location : {
+        type:String,
+        required:true 
+    },
+    feature:{
+        type:Boolean,
+    },
+    prerequisites : [{
+        type:String,
+        required:true 
+}],
+    // syllabus : {
+    //     type:String,
+    //     required:true 
+    // },
+    syllabus : [
+        {
+            week:{
+                type:String,
+            },
+            topic:{type:String,
+            },
+            content:{
+                type:String,
+            }
+        }
+    ],
+
+    createdAt:{
+        type:Date,
+        default: Date.now()
+    }
+})
+
+module.exports = mongoose.model('Product', productSchema);
